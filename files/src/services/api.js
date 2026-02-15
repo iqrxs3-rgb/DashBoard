e06bd99b073d4152b540fb44d9096e51a49c3e2d
iqrxs3-rgb <iqrxs3@gmail.com>
2026-02-16 00:26:59 +0300

    Alex

--- files/src/services/api.js
+++ files/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { useAuthStore } from '../store/authStore'
 
-const API_BASE_URL = 'http://localhost:3001/api'
+const API_BASE_URL = 'https://backendbe.up.railway.app/'
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
